41c93a1a3a6bac6f08fa1b601f5bdf28679012fa
David Carley <dave.carley@smartsocialmedia.com>
2015-03-16 14:45:51 -0700

    Improved the unit tests.

--- test/test.js
+++ test/test.js
@@ -1,36 +1,57 @@
 var expect = require('chai').expect;
 
 describe("simple-di", function() {
-  var di = require('../index.js');
+    var di;
 
-  it("should not find a module that isn't defined", function() {
-    var circle = di.get('Circle');
-    expect(circle).to.not.exist;
-  });
+    beforeEach(function() {
+        // Ensure that the 'simple-di' module is reloaded before each test.
+        delete require.cache[require.resolve('../index.js')];
+        di = require('../index.js');
+    });
 
-  it("should be able to instantiate a valid dependency graph", function() {
-    di.load("../examples/basic/**/*.js", ["../examples/basic/app.js", "../examples/basic/ignore_this_folder/**/*"]);
-    var circle = di.get('Circle');
-    expect(circle).to.exist;
-  });
+    it("should not find a module that isn't defined", function() {
+        var circle = di.get('Circle');
+        expect(circle).to.not.exist;
+    });
 
-  it("should throw an exception if there is a circular dependency", function() {
-    di.load("../examples/circular/**/*.js", "../examples/circular/app.js");
-    
-    function getA() {
-      var A = di.get('A');
-    }
+    it("should be able to instantiate a valid dependency graph", function() {
+        di.load("../examples/basic/**/*.js", ["../examples/basic/app.js", "../examples/basic/ignore_this_folder/**/*"]);
+        var circle = di.get('Circle');
+        expect(circle).to.exist;
+    });
 
-    expect(getA).to.throw(Error);
-  });
+    it("should throw an exception if there is a circular dependency", function() {
+        di.load("../examples/circular/**/*.js", "../examples/circular/app.js");
+
+        function getA() {
+            var A = di.get('A');
+        }
+
+        expect(getA).to.throw(Error);
+    });
 
     it("should throw an exception if there is an unresolved dependency", function() {
-    di.load("../examples/unresolved/**/*.js", "../examples/unresolved/app.js");
-    
-    function getA() {
-      var A = di.get('A');
-    }
+        var A = di.get('A');
+        expect(A).to.not.exist;
 
-    expect(getA).to.throw(Error);
-  });
+        di.load("../examples/unresolvable/**/*.js", "../examples/unresolvable/app.js");
+
+        function getA() {
+            var A = di.get('A');
+        }
+
+        expect(getA).to.throw(Error);
+    });
+
+    it("should throw an exception if the same module is declared multiple times", function() {
+        var A = di.get('A');
+        expect(A).to.not.exist;
+
+        function register_two() {
+            di.declare('A', function() {});
+            di.declare('A', function() {});
+        }
+
+        expect(register_two).to.throw(Error);
+    });
 });
